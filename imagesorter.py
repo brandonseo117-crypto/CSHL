af6e771fd37790cd51aefd44a777fc0c226fa297
Brandon <brandonseo117@gmail.com>
2026-07-13 15:32:33 -0400

    sorted by hsv not raw rgb for perceptual insight

--- imagesorter.py
+++ imagesorter.py
@@ -2,21 +2,32 @@ from pathlib import Path
 from PIL import Image, ImageStat
 from dotenv import load_dotenv
 import os
+import colorsys
 
 load_dotenv()
 folder_path = os.getenv('IMAGE_FOLDER_PATH')
 image_list = []
 
 
+def color_score(r, g, b, target_hue_deg):
+    h, s, v = colorsys.rgb_to_hsv(r, g, b)
+    hue_deg = h * 360
+    hue_dist = min(abs(hue_deg - target_hue_deg),
+                   360 - abs(hue_deg - target_hue_deg))
+    hue_match = 1 - (hue_dist / 180)
+    return hue_match * s * v
+
+
 def color_stats(image):
     with Image.open(image).convert('RGB') as img:
         stats = ImageStat.Stat(img)
+        r, g, b = [x / 255.0 for x in stats.mean]
 
-        avg_r = stats.mean[0]
-        avg_g = stats.mean[1]
-        avg_b = stats.mean[2]
+    red = color_score(r, g, b, 0)
+    green = color_score(r, g, b, 120)
+    blue = color_score(r, g, b, 240)
 
-        return avg_r, avg_g, avg_b
+    return red, green, blue
 
 
 class StimulusImage:
